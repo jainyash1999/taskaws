eabef9506d10411d87ba85ffc52fe25e46b1971f
jainyash1999 <yashlovingly@gmail.com>
2020-06-13 19:07:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-this is a my website
-<img src="https://yashbuckettca1751023.s3.ap-south-1.amazomaws.com/terraform.png"/>
-<pre>
-<?php
-print`ifconfig`;
-echo "hello";
-?>
-</pre>
+<html>
+<body>
+<h1>WELCOME TO MY WEBSITE</h1>
+<h3>lets have fun</h3>
+ <?php
+    $cloudant_url=`head -n1 path.txt`;
+    $img_path="https://".$cloudant_url."/terraform.png";
+    echo "<br>";
+    echo "<img src='{img_path}' width=100 height=100>";
+ ?>
+</body> 
+</html> 
