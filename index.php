a5d5fa975d78d3ef6db0a61947a3650fe827ce72
jainyash1999 <yashlovingly@gmail.com>
2020-06-14 22:10:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
    $cloudant_url=`head -n1 path.txt`;
    $img_path="https://".$cloudant_url."/terraform.png";
    echo "<br>";
-   echo "<img src='{img_path}' width=100 height=100>";
+   echo "<img src='${img_path}' width=100 height=100>";
 ?>
 </body> 
 </html> 
